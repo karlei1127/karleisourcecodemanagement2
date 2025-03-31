fb1967a4121555957187ad6acea68372369ec32d
smith71119 <smith71119@B124-21>
2025-03-31 10:30:51 -0400

    changed the ints to floats, created a function definition, added exiting condition, changed the format a little

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,28 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+float average(float, float, float); 
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
-	cout << "This program calculates the average of three numbers." << endl;
+	float n1 =0 , n2 = 0, n3 = 0;
+	cout << "\n\tThis program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
 	cout << "Third number: "; cin >> n2;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
 
+	cout << setprecision(1) << fixed << "The average is: " << a << endl;
+
+	cout << "Press enter to exit.";
+	cin.ignore(); 
+	cin.get(); 
 	return 0;
+}
+
+
+float average(float i1, float i2, float i3){
+	return i1 + i2 + i3 / 3.0f;
 }
